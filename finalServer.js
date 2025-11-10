79a321e13f818cea88decf1bf7ea2f9fb5ae8134
wilfred <wilfredmvula@icloud.com>
2025-11-10 11:11:52 +0200

    added the s3

--- finalServer.js
+++ finalServer.js
@@ -11,7 +11,8 @@ app.use(express.json());
 app.use(cors({
   origin: [
     "http://localhost:5173", 
-    "http://www.petalperfect.com.s3-website-us-east-1.amazonaws.com"
+    "http://www.petalperfect.com.s3-website-us-east-1.amazonaws.com",
+    "http://www.petalperfect.co.za.s3-website-us-east-1.amazonaws.com"
   ],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
