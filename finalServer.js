0570e3228eae8111ecf65dde072645db13cbe7d8
wilfred <wilfredmvula@icloud.com>
2025-11-10 13:26:57 +0200

    fix issues

--- finalServer.js
+++ finalServer.js
@@ -19,12 +19,7 @@ app.use(cors({
   credentials: true
 }));
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
+
 
 let client, db;
 
